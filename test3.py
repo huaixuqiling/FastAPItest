7026ab178b67604d285675dda776f0b87f6b010f
HuaiXuQiLing <cqwm@qq.com>
2026-08-17 20:56:00 +0800

    FastAPItest

--- test3.py
+++ test3.py
@@ -1,16 +1,14 @@
 from datetime import datetime
-from unittest import result
-
-from select import select
-from sqlalchemy import func, DateTime, String, Float
-from fastapi import FastAPI,Query,Depends
+from sqlalchemy import func, DateTime, String, Float,select,funcfilter
+from fastapi import FastAPI,Query,Depends,HTTPException
 from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker,async_session,AsyncSession
 from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
+from pydantic import BaseModel
 
 app=FastAPI()
 
 #创建异步引擎
-ASYNC_DATABASE_URL="mysql+aiomysql://root:111111@localhost:3306/FastAPI_test?charset=utf8"
+ASYNC_DATABASE_URL="mysql+aiomysql://root:111111@localhost:3306/FastAPI_test?charset=utf8mb4"
 async_engine=create_async_engine(
     ASYNC_DATABASE_URL
     ,echo=True #可选,输出数据库日志
@@ -46,7 +44,7 @@ async def startup_event():
 #查询图书接口 依赖注入
 AsyncSessionLocal = async_sessionmaker(
     bind=async_engine #绑定数据库引擎
-    , class_=async_session #指定会话类
+    , class_=AsyncSession #指定会话类
     , expire_on_commit=False #可选,提交后不过期
 )
 async def get_session():
@@ -62,8 +60,114 @@ async def get_session():
 
 @app.get("/book/getbooks")
 async def get_books_list(db: AsyncSession = Depends(get_session)):
-    #查询
-    result = await db.execute(select(Book))
-    return result.scalars().all()
+    #查询所有
+    result = await db.execute(select(Book)) #查询返回ORM对象
+    book = result.scalars().all()    #获取所有
+    book = result.scalars().first() #获取第一个数据
+    return book
+    # #查询单条
+    # book = await db.get(Book,3)  #根据主键获取单条
+    # return book  #返回
+#条件查询
+@app.get("/book/getbooks/{book_id}")
+async def getbook(book_id: int,db:AsyncSession = Depends(get_session)):
+    result=await db.execute(select(Book).where(Book.id==book_id))
+    book = result.scalar_one_or_none()
+    return book
+
+@app.get("/book/getbook/money")
+async def get_money(db:AsyncSession = Depends(get_session)):
+    result=await db.execute(select(Book).where(Book.price>=50))
+    book_money=result.scalars().all()
+    return book_money
+
+#模糊查询
+@app.get("/book/getbook/name")
+async def get_author(db:AsyncSession = Depends(get_session)):
+    ##like模糊查询%匹配任意，_匹配一个  需求：查询作者为吴*的书籍，或者价格大于50的书籍
+    # result=await db.execute(select(Book).where(Book.author.like("吴%")))
+    ## &|，与或非条件，需求：查询作者为吴*的书籍，或者价格大于50的书籍
+    # result=await db.execute(select(Book).where((Book.author.like("吴%"))|(Book.price>=50)))
+    #in,包含需求：书籍id列表，在id列表里面，返回
+    id_list=[1,2,5,7]
+    result = await db.execute(select(Book).where(Book.id.in_(id_list) ))
+    book_author=result.scalars().all()
+    return book_author
+
+#聚合查询   func.方法名(模型类.属性)  需求：查询书籍价格的总和sum，平均值avg，最大值max，最小值min,行数 count
+@app.get("/book/getbookmoney")
+async def get_bookmoney(db:AsyncSession = Depends(get_session)):
+    # result=await db.execute(select(func.count(Book.price)))
+    # result=await db.execute(select(func.max(Book.price)))
+    # result=await db.execute(select(func.sum(Book.price)))
+    result = await db.execute(select(func.avg(Book.price)))
+    book_money=result.scalar()
+    return book_money
+
+#分页查询
+@app.get("/book/pagebook")
+async def get_pagebook(
+        page:int = Query(1, title="页码"),
+        size:int = Query(2, title="每页数量"),
+        db:AsyncSession = Depends(get_session)
+):
+    offset=(page-1)*size
+    #offset跳过的记录数 limit每页记录数
+    result=await db.execute(select(Book).offset(offset).limit(size))
+    book_page=result.scalars().all()
+    return book_page
 
 
+class BookBase(BaseModel):
+    id: int
+    name: str
+    author: str
+    price: float
+    publisher: str
+
+#新增数据
+@app.post("/book/addbook")
+async def addbook(book:BookBase,db:AsyncSession = Depends(get_session)):
+    book_obj=Book(**book.__dict__)
+    db.add(book_obj)
+    await db.commit()
+    return book
+
+class BookUpdate(BaseModel):
+    name: str
+    author: str
+    price: float
+    publisher: str
+
+#更新数据/修改/先查再改
+@app.put("/book/updatebook/{bookid}")
+async def updatebook(
+        bookid:int,
+        data:BookUpdate,
+        db:AsyncSession = Depends(get_session)
+):
+    db_book=await db.get(Book,bookid)
+    if db_book is None:
+        raise HTTPException(
+            status_code=404,
+            detail="概书籍未找到，请重试"
+        )
+    db_book.name=data.name
+    db_book.author=data.author
+    db_book.price=data.price
+    db_book.publisher=data.publisher
+    await db.commit()
+    return db_book
+
+#删除图书
+@app.delete("/book/deletebook/{bookid}")
+async def deletebook(bookid:int,db:AsyncSession = Depends(get_session)):
+    db_book=await db.get(Book,bookid)
+    if db_book is None:
+        raise HTTPException(
+            status_code=404,
+            detail="概书籍未找到，请重试"
+        )
+    await db.delete(db_book)
+    await db.commit()
+    return {"message":"删除成功"}
